7bcc6620dc1f0d2a6ba7b15f68f17f1d5deeba15
brm-technology <great.bahram@gmail.com>
2024-12-02 09:31:51 +0100

    Update settings.py

--- be/brmproj/settings.py
+++ be/brmproj/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'brmproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'fe'],
+        'DIRS': [BASE_DIR / 'static'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
